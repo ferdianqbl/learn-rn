022507570902d2aeecddbf31aa0bf6ae812042be
ferdianqbl <muhammadferdianiqbal@gmail.com>
2024-01-20 11:17:57 +0700

    try to change vieww

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={{ width: 50, height: 50, backgroundColor: "red" }} />
+        <View style={{ width: 100, height: 100, backgroundColor: "blue" }} />
+        <View style={{ width: 150, height: 150, backgroundColor: "green" }} />
+        <View style={{ width: 200, height: 200, backgroundColor: "yellow" }} />
+        <View style={{ width: 250, height: 250, backgroundColor: "orange" }} />
+        <View style={{ width: 300, height: 300, backgroundColor: "pink" }} />
+      </View>
+      {/* <StatusBar style="auto" /> */}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingVertical: 50,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
 });
